25aeaf24f24b3f79d13de875140568c4ad1756cb
Trigs <mtrigsauer@gmail.com>
2018-06-07 09:45:01 -0300

    Basic parallaxing, player respawn and basic camera follow.

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+	public static GameMaster gm;
+	Vector3 spawnPosition;
+	public Transform playerPrefab;
+	public Transform spawnPoint;
+
+	void Start () {
+		if (gm == null) {
+			gm = GameObject.FindGameObjectWithTag ("GM").GetComponent<GameMaster>();
+			spawnPosition.Set (spawnPoint.position.x, spawnPoint.position.y, 0);
+		}
+	}
+
+	//public int spawnDelay;
+
+	public void RespawnPlayer () {
+		playerPrefab.transform.position = spawnPosition;
+		for (int i = 0; i < playerPrefab.transform.childCount; i++) {
+			playerPrefab.transform.GetChild (i).transform.position = spawnPosition;
+		}
+		Debug.Log (playerPrefab.transform.position + " PlayerPrefab");
+		Debug.Log (spawnPosition + " SpawnPosition");
+		Debug.Log ("TO DO: Add Spawn Particles");
+	}
+
+	/*
+	public static void KillPlayer (PlayerComponent player) {
+		Destroy (player.gameObject);
+		gm.RespawnPlayer ();
+	}
+	*/
+
+}
---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour {
+
+	/*
+	[System.Serializable]
+	public class PlayerStats {
+		public int Health = 100;
+	}
+
+	public PlayerStats playerStats = new PlayerStats(); 
+
+
+	public int fallBoundary = -20;
+
+	void Update () {
+		if (transform.position.y <= fallBoundary) {
+			DamagePlayer (9999999);
+		}
+	}
+
+	public void DamagePlayer (int damage) {
+		playerStats.Health -= damage;
+		if (playerStats.Health <= 0) {
+			Debug.Log ("KILL PLAYER");
+			GameMaster.KillPlayer (this);
+		}
+	}
+	*/
+
+
+	void OnCollisionEnter2D (Collision2D info) {
+		if (info.collider.tag == "Danger") {
+			Debug.Log ("YOU ARE DEAD");
+			gameObject.SetActive (false);
+			//Destroy (this);
+			GameMaster.gm.RespawnPlayer ();
+			gameObject.SetActive (true);
+		}
+			
+	}
+
+}
