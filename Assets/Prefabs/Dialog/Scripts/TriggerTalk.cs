87499ea8eeb5fa8426789740ea503644f0e44934
mlhomsi <mlhomsi@gmail.com>
2018-08-29 07:28:32 -0300

    Teste de Level

--- Assets/Prefabs/Dialog/Scripts/TriggerTalk.cs
+++ Assets/Prefabs/Dialog/Scripts/TriggerTalk.cs
@@ -11,6 +11,7 @@ public class TriggerTalk : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("as");
         if (!talkActivated)
         {
             talkActivated = true;
