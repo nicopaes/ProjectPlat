8546f1c9c0b9b7b9fdcfdc161e1c3584474cdd51
Mabel <mabelmariz@gmail.com>
2018-09-13 11:34:47 -0300

    Menu brubs (provisorio)

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void ChangeSingleScene(string name)
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+
+    public void ChangeAdditiveScene(string name){
+        SceneManager.LoadScene(name, LoadSceneMode.Additive);
+        Time.timeScale = 0f;
+    }
+
+    public void UnloadScene(string name){
+        SceneManager.UnloadSceneAsync(name);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+}
