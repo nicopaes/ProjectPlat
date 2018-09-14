697b9dc6a63a2d397260f30ad1ac1e1defb499c3
Mabel <mabelmariz@gmail.com>
2018-09-14 09:24:50 -0300

    pause working

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -12,17 +12,20 @@ public class ChangeScene : MonoBehaviour
     }
 
     public void ChangeAdditiveScene(string name){
-        SceneManager.LoadScene(name, LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
         Time.timeScale = 0f;
     }
 
     public void UnloadScene(string name){
+        if(name == "pause"){
+            GameObject.Find("SceneManager").GetComponent<PauseManager>().SetIsPaused();
+        }
         SceneManager.UnloadSceneAsync(name);
         Time.timeScale = 1f;
     }
 
-    public void QuitGame(){
+    public void QuitGame()
+    {
         Application.Quit();
     }
-
 }
