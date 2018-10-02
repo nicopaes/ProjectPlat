ba88a6aabc82281d0274f32b1db1b7e883522fa1
Matheus Homsi <mlhomsi@gmail.com>
2018-10-02 13:22:31 -0300

    Gambiarra da cena da dose pt.1

---
+++ Assets/FreezePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezePlayer : MonoBehaviour {
+
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("teje parado");
+        Player.GetComponent<InputController>().enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
