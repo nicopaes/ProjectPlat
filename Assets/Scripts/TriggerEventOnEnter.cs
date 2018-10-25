cfbd46a0c73f664c4915f81939cb2b06426ee98e
Andre Mazal Krauss <amk1710@gmail.com>
2018-10-25 12:43:14 -0300

    trigger de passar pra proxima fase

---
+++ Assets/Scripts/TriggerEventOnEnter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventOnEnter : MonoBehaviour {
+
+	public UnityEvent Event;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")){
+
+            //aciona evento
+			Event.Invoke();
+			
+			// //se player está interagindo OU eu o dialogo deve começar por proximidade,
+            // if(collision.GetComponent<PlayerComponent>().isOnDialogTrigger || StartOnApproach){
+
+            //     ChooseCamera();
+            //     activateDialog();
+
+            // }
+        }
+    }
+}
