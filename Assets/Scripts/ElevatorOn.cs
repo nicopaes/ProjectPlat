2f5b8e32d2fcb6b28136b219a7d2593a947a869e
Mabel <mabelmariz@gmail.com>
2018-10-26 13:15:33 -0300

    Arte dos setores, animacao luzes setores, animação do elevador (shake), porta abrindo do elevador

---
+++ Assets/Scripts/ElevatorOn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorOn : MonoBehaviour
+{
+
+    public Animator camAnimator;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("aloalo");
+        camAnimator.SetTrigger("elevatorOn");
+    }
+}
