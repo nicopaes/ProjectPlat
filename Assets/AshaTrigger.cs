d591ae41c3251be447efdbc1c37b9e7a0615cc97
Daniel Oliveira <thedannyvegas@gmail.com>
2018-11-17 05:41:00 -0200

    Trigger da animação de dose implementado

---
+++ Assets/AshaTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AshaTrigger : MonoBehaviour {
+
+	public Animator anim;
+
+	void TriggerAnimation (){
+		anim.SetTrigger("dose");
+	}
+}
