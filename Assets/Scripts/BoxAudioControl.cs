d2596b875604f075a95dd0270125a802c0dda872
Andre Mazal Krauss <amk1710@gmail.com>
2018-11-29 12:58:19 -0200

    som na caixa (dj)

---
+++ Assets/Scripts/BoxAudioControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BoxAudioControl : MonoBehaviour {
+
+	public AudioSource audioSource;
+	//[Tooltip("O volume desejado pra quando a caixa ser arrastada")]
+	private float normalVolume;
+	private Rigidbody2D rb;
+
+	
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		normalVolume = audioSource.volume;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Mathf.Abs(rb.velocity.x) > 0)
+		{
+			audioSource.volume = normalVolume;	
+		}
+		else
+		{
+			audioSource.volume = 0.0f;
+		}
+		
+	}
+}
