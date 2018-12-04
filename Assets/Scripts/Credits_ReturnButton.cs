a82465f6265aa1a2a5f3bf35e4a321bff5604142
Andre Mazal Krauss <amk1710@gmail.com>
2018-12-03 22:29:38 -0200

    ativar o mouse e botão de voltar lá no final dos creditos

---
+++ Assets/Scripts/Credits_ReturnButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Credits_ReturnButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnEnable()
+	{
+		Cursor.visible = true;
+		EventSystem.current.SetSelectedGameObject(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
